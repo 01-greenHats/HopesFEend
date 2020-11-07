69a6da0ff68c7a764a97145d49cb185251879136
Ahmad Shela <ahmedsh717@gmail.com>
2020-11-07 03:12:07 +0200

    add commetns

--- src/components/commentCard/index.js
+++ src/components/commentCard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './commentCard.scss'
+import './commentCard.scss';
 
 function CommentCard(props){
     return(
@@ -7,7 +7,7 @@ function CommentCard(props){
         <div className="commentCard" style={{display:"flex",marginLeft:"15px",padding:"5px"}}>
         <img src={`${props.comment.imgURL}`} alt="Avatar" style={{width:"30px",borderRadius:"50%",padding:"7px 0px"}}/>
         <div>
-    <p style={{fontSize:"75%",fontWeight:"bold",marginLeft:"3px"}}>{props.comment.name}</p>
+        <p style={{fontSize:"75%",fontWeight:"bold",marginLeft:"3px"}}>{props.comment.name}</p>
         <div style={{border:"1px solid black"}} className="commentText">
             <p style={{margin:"10px"}}>{props.comment.content}</p>
             </div>
