a4a67d942c8124b99c36974f541001e55fd69623
Your Name <you@example.com>
2020-11-08 15:17:54 +0200

    adding things

--- src/app.js
+++ src/app.js
@@ -2,41 +2,43 @@ import React from 'react';
 
 import './styles.scss';
 import InNeedUsers from './components/inNeedUsers';
-// import Header from './components/header';
+import PersonalProfile from './components/personalProfile';
+import Header from './components/header';
 import { Route, Switch } from 'react-router-dom';
 import MainPage from './components/mainPage';
 import SiginupUser from './components/signupUser';
 import Login from './components/loginUser';
 import DonorRegisteration from './components/donorForms'
 import UserPayments from './components/userPaymsDtl';
+import Loader from './components/loader';
+
 
 
 export default propsimport => {
   return (
     <div id='main'>
+      <Header />
       <Switch>
         <Route exact path="/in_need_users">
-            <InNeedUsers/>
+          <InNeedUsers />
         </Route>
-
-
-
-
         <Route exact path="/user_payments/:id" component={UserPayments}>
         </Route>
-
-
         <Route exact path="/">
-            <MainPage/>
+          <MainPage />
+          <Loader />
         </Route>
         <Route exact path="/signupUser">
-          <SiginupUser/>
+          <SiginupUser />
         </Route>
         <Route exact path="/loginUser">
-          <Login/>
+          <Login />
         </Route>
         <Route exact path="/login-signup-donor">
-          <DonorRegisteration/>
+          <DonorRegisteration />
+        </Route>
+        <Route exact path="/personal_profile">
+          <PersonalProfile />
         </Route>
         <Route>404 Page Not Found!</Route>
       </Switch>
---
+++ src/components/personalProfile/index.js
@@ -0,0 +1,14 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+
+export default function Header() {
+
+  return (
+    <main>
+        <Container>
+
+        </Container>
+    </main>
+  );
+}
