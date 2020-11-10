bc91386cd93ae54f772f6cb6081841a1528e0ad9
ahmed <ahmedsh717@gmail.com>
2020-11-10 12:30:17 +0200

    fix the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './header.scss'
 import Logo from './logo.png';
 import { Link } from "react-router-dom";
+import { If,Then,Else } from '../if';
 
 
-export default function Header() {
+function Header(props) {
 
   return (
     <header>
@@ -13,9 +15,26 @@ export default function Header() {
         <ul>
           <li class='active'><Link to='/'>Home</Link></li>
           <li class='active'><Link to='/in_need_users'>Donate</Link></li>
+          <If condition={props.loggedIn &&  props.userType == 'users'}>
+            <Then>
           <li class='active'><Link to='/personal_profile'>Profile</Link></li>
-          <li class='active'><Link to='/userForm'>LogIn</Link></li>
-          <li class='active'><Link to='/userForm'>SignUp</Link></li>
+            </Then>
+          </If>
+          <If condition={!props.loggedIn}>
+            <Then>
+          <li class='active'><Link to='/userForm'>LogIn/SignUp</Link></li>
+            </Then>
+          </If>
+          <If condition={!props.loggedIn}>
+            <Then>
+          <li class='active'><Link to='/login-signup-donor'>LogIn/SignUp As Donor</Link></li>
+            </Then>
+          </If>
+          <If condition={props.loggedIn &&  props.userType == 'donors'}>
+            <Then>
+            <li class='active'><Link to='/about-us'>My List</Link></li>
+            </Then>
+          </If>
           <li class='active'><Link to='/about-us'>About us</Link></li>
         </ul>
       </div>
@@ -23,3 +42,11 @@ export default function Header() {
     
   );
 }
+const mapStateToProps = state => (
+  {
+      userType: state.auth.user.userType,
+      loggedIn: state.auth.loggedIn
+  }
+);
+// const mapDispatchToProps = { checkIsLogedIn };
+export default connect(mapStateToProps)(Header);
